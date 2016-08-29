4d085ada0d7e6bc5f32a1723c23109873cd175b8
Luca Morreale <luca.morreale93@gmail.com>
2016-08-30 01:05:53 +0200

    Fixed DataConverter static class

--- code/basestation/data_converter.cpp
+++ code/basestation/data_converter.cpp
@@ -1,55 +1,83 @@
 
 #include "data_converter.h"
-#include <string>
-#include <algorithm>
-#include <iterator>
-#include <regex>
-#include <vector>
+
+using namespace basestation;
+
 
 const std::string DataConverter::EMPTY = "NONE";
 const std::string DataConverter::ERROR = "ERR";
 const std::string DataConverter::VALID = "FOUND";
-
-/*
-    FOUND,<id tag>, <battery>,
-    <RSSI 125>, <id tag>,
-    <battery>, <RSSI 125> ...
-    DONE - NONE
-*/
-
+std::regex DataConverter::reg = std::regex("<\\w+>");
 
 
 std::string DataConverter::convertToJson(std::string str)
 {
+
     if(isEmpty(str)) {
-
+        return DataConverter::getEmptyJson().toStyledString();
     } else if (isError(str)) {
-
+        return DataConverter::getErrorJson().toStyledString();
     } else {
-        
-        std::copy(std::sregex_token_iterator(str.begin(), str.end(), "\\s+", -1),
-              std::sregex_token_iterator(),
-              std::vector<std::string>());
+        return DataConverter::parse(str).toStyledString();
     }
 }
 
 
-std::string DataConverter::isValid(std::string str)
+bool DataConverter::isValid(std::string str)
 {
     std::string tmp = trim(str);
-    return startsWith(str, VALID);    
+    return DataConverter::startsWith(str, VALID);    
 }
 
-std::string DataConverter::isEmpty(std::string str)
+bool DataConverter::isEmpty(std::string str)
 {
     std::string tmp = trim(str);
-    return startsWith(str, EMPTY);  
+    return DataConverter::startsWith(str, EMPTY);  
 }
 
-std::string DataConverter::isError(std::string str)
+bool DataConverter::isError(std::string str)
 {
     std::string tmp = trim(str);
-    return startsWith(str, ERROR);  
+    return DataConverter::startsWith(str, ERROR);  
+}
+
+Json::Value DataConverter::getEmptyJson()
+{
+    Json::Value root;
+    root["beacons"] = "";
+
+    return root;
+}
+Json::Value DataConverter::getErrorJson()
+{
+    Json::Value root;
+    root["beacons"] = "ERR";
+
+    return root;
+}
+
+Json::Value DataConverter::parse(std::string str)
+{
+    VectorString data;
+    std::smatch match;
+
+    Json::Value root;
+    Json::Value vec(Json::arrayValue);
+
+    //std::regex_search(str, self_regex)
+    std::regex_match(str, match, reg);
+
+    for(int i = 0; i < match.size(); i += 3) {
+        Json::Value beacon;
+        beacon["id_tag"] = match.str(i);
+        beacon["rssid"] = match.str(i+2);
+
+        vec.append(beacon);
+    }
+
+    root["beacons"] = vec;
+
+    return root;
 }
 
 std::string DataConverter::trim(std::string str)
@@ -62,5 +90,5 @@ std::string DataConverter::trim(std::string str)
 bool DataConverter::startsWith(std::string str, std::string start)
 {
     std::size_t found = str.find(start);
-    return found!=std::string::npos && found == 0;
+    return found != std::string::npos && found == 0;
 }
