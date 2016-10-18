1c389b797e4f3f52bef0e017f1584039d27e0e00
Luca Morreale <luca.morreale93@gmail.com>
2016-10-18 15:20:00 +0200

    Fixed client

--- code/client_python/client.py
+++ code/client_python/client.py
@@ -5,15 +5,15 @@ import sys
 class Client():
 
     DEFAULT_BUFFER_SIZE = 1024
-    SERVER_IP_ADDR = '192.168.1.19'
     SERVER_PORT = 10019
 
     def __init__(self, port=SERVER_PORT):
         self.port = port
 
     def pollBasestation(self, address):
-        return Client.poll(port, address)
+        return Client.poll(address, self.port)
 
+    @staticmethod
     def poll(address, port):
         #Create a TCP/IP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,15 +29,13 @@ class Client():
             print ('Start reading from server')
 
             # Look for the response
-            data = sock.recv(DEFAULT_BUFFER_SIZE)
+            data = sock.recv(Client.DEFAULT_BUFFER_SIZE)
             if(not data == 0):
                 print ("Received data: %s, (%d bytes)" %(data, len(data)))
+                return data
 
             else:
-                print("Connection is broken")
+                return Exception()
 
         finally:
-            print ("Closing socket")
             sock.close()
-
-    
