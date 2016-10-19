f92a1141a6d96d3f458984101d3a54bbd944bd95
Luca Morreale <luca.morreale93@gmail.com>
2016-10-19 15:35:02 +0200

    Added function fo extract first element

--- code/client_python/json_extractor.py
+++ code/client_python/json_extractor.py
@@ -5,4 +5,11 @@ def extractJson(data):
 		return {}
 
 	beacons = json.loads(data)
+	return beacons["beacons"]
+
+def extractFirstRSSI(data):
+	if len(data) <= 0:
+		return -1
+
+	beacons = json.loads(data)
 	return beacons["beacons"][0]['rssi']
