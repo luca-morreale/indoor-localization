90713e64ba1e70fc3c098fe107ba7d983fb4179f
Luca Morreale <luca.morreale93@gmail.com>
2016-10-23 11:41:32 +0200

    Refactored names

--- code/client_python/ekf.py
+++ code/client_python/ekf.py
@@ -1,17 +1,17 @@
 import numpy as np
 from client import Client
-from json_extractor import extractJson
+from json_extractor import extractRSSIForTag
 
 
-class EKF():
+class EKF(object):
 
-    def __init__(self,dt,sensor_size,var_z,beacons,coeff):
-        self.client = Client(10019)
-        self.coeff = coeff
-        self.beacons = beacons  # of type Basestation
+    def __init__(self, tag, dt, sensor_size, var_z, beacons, coeff):
+        self.tag = tag
         self.dt = dt
         self.sensor_size = sensor_size
-        self.accel_noise_mag = 0.001**2/dt
+        self.beacons = beacons  # of type Basestation
+        self.model_coeffs = coeff
+        self.client = Client(10019)
         self.Ex = np.array([[dt**3/3, 0,       dt**2/2, 0],
                             [0,       dt**3/3, 0,       dt**2/2],
                             [dt**2/2, 0,       dt,      0],
@@ -20,77 +20,70 @@ class EKF():
                             [0, 1, 0, dt], 
                             [0, 0, 1, 0], 
                             [0, 0, 0, 1]])
-        self.x_hat = np.empty((0, 4)) #shape is (1,4) - to init with initial position
+        self.estimated_position = np.empty((0, 4))  # shape is (1,4) - to init with initial position
         self.Ez = np.eye(N=sensor_size, M=sensor_size) * var_z
-        self.P = self.Ex
-        self.prediction = np.transpose(self.x_hat)
-
+        self.cov_matrix = self.Ex
+        self.prediction_sequence = [np.transpose(self.estimated_position)]
 
     def ekf(self):
-        H, h, measurements, P_hat = self.prediction()
-        self.correction(H, h, measurements, P_hat)
-
+        H, h, measurements, estimated_cov_matrix = self.prediction()
+        self.correction(H, h, measurements, estimated_cov_matrix)
+        return self.estimated_position
 
     def prediction(self):
-        self.x_hat = self.F * self.x_hat
-        P_hat = self.F * self.P * np.transpose(self.F) + self.Ex
-        measurements = self.selective_extraction(P_hat)
+        self.estimated_position = self.F * self.estimated_position
+        estimated_cov_matrix = self.F * self.cov_matrix * np.transpose(self.F) + self.Ex
+        measurements = self.selective_extraction(estimated_cov_matrix)
+
         h = np.zeros(self.sensor_size)
         for i in range(0, self.sensor_size):
-            if (measurements[i] != 0):
-                #h[i] = distance(self.x_hat[0],self.beacons[i])
-                h[i] = self.space_to_value(self.x_hat[0] - self.beacons[i].position)
+            if measurements[i] != 0:
+                h[i] = self.space_to_value(self.estimated_position[0] - self.beacons[i].position)
 
-        #H is the measurement equation
+        # H is the measurement equation
         H = np.empty((0, 4))
         for i in range(0, len(measurements)):
-            if (measurements[i] != 0):
-                dh_dx, dh_dy = self.derivative(self.x_hat[0] - self.beacons[i].position)
+            if measurements[i] != 0:
+                dh_dx, dh_dy = self.derivative(self.estimated_position[0] - self.beacons[i].position)
             else:
-                dh_dx = 0.0
-                dh_dy = 0.0
+                dh_dx, dh_dy = 0.0, 0.0
             H = np.append(H, np.array([[dh_dx, dh_dy, 0.0, 0.0]]), axis=0)
 
-        return H, h, measurements, P_hat
+        return H, h, measurements, estimated_cov_matrix
 
+    def correction(self, H, h, measurements, estimated_cov_matrix):
+        K = estimated_cov_matrix * np.transpose(H) * np.invert(H * estimated_cov_matrix * np.transpose(H) + self.Ez)
+        self.estimated_position += K * (measurements - h)
+        self.cov_matrix = (np.eye(N=4) - K * H) * estimated_cov_matrix
+        self.prediction_sequence.append(np.transpose(self.estimated_position))
 
-    def correction(self, H, h, measurements, P_hat):
-        K = P_hat * np.transpose(H) * np.invert(H * P_hat * np.transpose(H) + self.Ez)
-        self.x_hat += K * (measurements - h)
-        self.P = (np.eye(N=4) - K * H) * P_hat
-        self.prediction = np.append(self.prediction, np.array([np.transpose(self.x_hat)]), axis=0)
-
-
-    def selective_extraction(self, P_hat):
-        indexes = self.position_selector(P_hat)
+    def selective_extraction(self, estimated_cov_matrix):
+        indexes = self.position_selector(estimated_cov_matrix)
         measurements = []
         for i in indexes:
             data = self.client.pollBasestation(self.beacons[i].address)
-            data = extractFirstRSSI(data)
-            measurements += [data]
+            measurements += [extractRSSIForTag(self.tag, data)]
         return measurements
 
-
-    def position_selector(self,P_hat):
-        P_hat_inv = np.invert(P_hat)
+    def position_selector(self, estimated_cov_matrix):
+        estimated_cov_matrix_inv = np.invert(estimated_cov_matrix)
         distances = []
         for i in self.sensor_size:
-            b_position = np.append(self.beacons[i].position, np.array([0,0]));
-            A = np.transpose(self.x_hat) - b_position
-            distances += A * (P_hat_inv * np.transpose(A))
+            extended_beacon_pos = np.append(self.beacons[i].position, np.array([0, 0]))
+            difference = np.transpose(self.estimated_position) - extended_beacon_pos
+            distances += difference * (estimated_cov_matrix_inv * np.transpose(difference))
 
         sorted_tuples = sorted(enumerate(distances), key=lambda x: x[1])
         valid_distances = [item for item in sorted_tuples if item[1] > 0]
-        return [valid_distances[i][0] for i in range(0,min(3,len(valid_distances)))]
-
+        return [valid_distances[i][0] for i in range(0, min(3, len(valid_distances)))]
 
     def derivative(self, arr):
-        dx = self.coeff[0] * 1.5 * np.sqrt(arr * arr) * 2 * arr[0] + self.coeff[1] * 2 * arr[0] + self.coeff[2] * arr[
-            0] * 1 / np.sqrt(arr * arr)
-        dy = self.coeff[0] * 1.5 * np.sqrt(arr * arr) * 2 * arr[1] + self.coeff[1] * 2 * arr[1] + self.coeff[2] * arr[
-            1] * 1 / np.sqrt(arr * arr)
-        return dx,dy
-
+        dx = self.model_coeffs[0] * 1.5 * np.sqrt(arr * arr) * 2 * arr[0] + self.model_coeffs[1] * 2 * arr[0] + \
+                    self.model_coeffs[2] * arr[0] * 1 / np.sqrt(arr * arr)
+        dy = self.model_coeffs[0] * 1.5 * np.sqrt(arr * arr) * 2 * arr[1] + self.model_coeffs[1] * 2 * arr[1] + \
+                    self.model_coeffs[2] * arr[1] * 1 / np.sqrt(arr * arr)
+        return dx, dy
 
     def space_to_value(self, arr):
-        return self.coeff[0] * (np.sqrt(arr*arr))**3 + self.coeff[1] * (arr * arr) + self.coeff[2] * np.sqrt(arr * arr) + self.coeff[4]
+        return self.model_coeffs[0] * (np.sqrt(arr * arr)) ** 3 + self.model_coeffs[1] * (arr * arr) + \
+                    self.model_coeffs[2] * np.sqrt(arr * arr) + self.model_coeffs[4]
