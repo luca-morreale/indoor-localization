8aa530b4e89a52c0a549bc590f62250cc501eee3
Luca Morreale <luca.morreale93@gmail.com>
2016-10-23 11:41:13 +0200

    Added more function to extract measurements

--- code/client_python/json_extractor.py
+++ code/client_python/json_extractor.py
@@ -1,15 +1,34 @@
 import json
 
-def extractJson(data):
-	if len(data) <= 0:
-		return {}
 
-	beacons = json.loads(data)
-	return beacons["beacons"]
+def extractJson(data):
+    if len(data) <= 0:
+        raise NoMeasurementException("")
+
+    beacons = json.loads(data)
+    return beacons["beacons"]
+
 
 def extractFirstRSSI(data):
-	if len(data) <= 0:
-		return -1
+    if len(data) <= 0:
+        raise NoMeasurementException("")
 
-	beacons = json.loads(data)
-	return beacons["beacons"][0]['rssi']
+    beacons = json.loads(data)
+    return beacons["beacons"][0]["rssi"]
+
+
+def extractRSSIForTag(data, tag):
+    beacons = extractJson(data)
+    return rssiForTag(beacons, tag)
+
+
+def rssiForTag(beacons, tag):
+    for el in enumerate(beacons):
+        if el[1, "id_tag"] == tag:
+            return el[1, "rssi"]
+    raise NoMeasurementException(tag)
+
+
+class NoMeasurementException(Exception):
+    def __init__(self, tag, text="No correspondance found for tag id "):
+        super(Exception, self).__init__(text + tag)
