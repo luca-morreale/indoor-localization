ef0a8c07668495d9a3ce0850a7962bf958e9ea55
Luca Morreale <luca.morreale93@gmail.com>
2016-10-29 18:36:09 +0200

    Removed print in client class

--- code/client_python/src/position_publisher/src/client.py
+++ code/client_python/src/position_publisher/src/client.py
@@ -21,19 +21,15 @@ class Client():
         #Connect the socket to server
         #server_address = (SERVER_IP_ADDR, SERVER_PORT) #the address is defined as a pair (host,port)
         server_address = (address, port)
-        print('Connecting to %s port %s' % server_address)
         sock.connect(server_address)
 
         try:
             #Client ready to read data from server
-            print ('Start reading from server')
-
+            
             # Look for the response
             data = sock.recv(Client.DEFAULT_BUFFER_SIZE)
             if(not data == 0):
-                print ("Received data: %s, (%d bytes)" %(data, len(data)))
                 return data
-
             else:
                 return ""
 
