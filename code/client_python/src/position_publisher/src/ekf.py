d20153c1a77e28ec50bfa29601e2d2faa27f5265
Luca Morreale <luca.morreale93@gmail.com>
2016-10-29 19:26:32 +0200

    Fixed ros node

--- code/client_python/src/position_publisher/src/ekf.py
+++ code/client_python/src/position_publisher/src/ekf.py
@@ -1,42 +1,49 @@
 import numpy as np
 from client import Client
 from json_extractor import extractRSSIForTag
+from matrix_operations import transpose, invert, multiply
 
 
 class EKF(object):
-
-    def __init__(self, tag, dt, sensor_size, var_z, beacons, coeff):
+    def __init__(self, tag, dt, var_z, beacons, model, selector):
         self.tag = tag
         self.dt = dt
-        self.sensor_size = sensor_size
+        self.var_z = var_z
+        self.sensor_size = len(beacons)
         self.beacons = beacons  # of type Basestation
-        self.model_coeffs = coeff
+        self.model = model
+        self.selector = selector
         self.client = Client(10019)
-        self.Ex = np.array([[dt**3/3, 0,       dt**2/2, 0],
-                            [0,       dt**3/3, 0,       dt**2/2],
-                            [dt**2/2, 0,       dt,      0],
-                            [0,       dt**2/2, 0,       dt]])
+        self.Ex = np.array([[dt ** 3 / 3, 0, dt ** 2 / 2, 0],
+                            [0, dt ** 3 / 3, 0, dt ** 2 / 2],
+                            [dt ** 2 / 2, 0, dt, 0],
+                            [0, dt ** 2 / 2, 0, dt]])
         self.F = np.array([[1, 0, dt, 0],
-                            [0, 1, 0, dt], 
-                            [0, 0, 1, 0], 
-                            [0, 0, 0, 1]])
-        self.estimated_position = np.empty((0, 4))  # shape is (1,4) - to init with initial position
-        self.Ez = np.eye(N=sensor_size, M=sensor_size) * var_z
+                           [0, 1, 0, dt],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+        self.initVariables()
+
+    def initVariables(self):
+        self.Ez = np.eye(N=self.sensor_size, M=self.sensor_size) * self.var_z
         self.cov_matrix = self.Ex
-        self.prediction_sequence = [np.transpose(self.estimated_position)]
+        self.prediction_sequence = []
 
     def setInitialPosition(self, init_pos):
-        for i in range(4):
+        self.initVariables()
+        self.estimated_position = np.zeros(4)
+        for i in range(0, 2):
             self.estimated_position[i] = init_pos[i]
+        self.prediction_sequence.append(self.estimated_position)
 
     def setInitialPositionToCloserBeacon(self):
         closer = self.getCloserBeacon()
-        self.setInitialPosition(closer.address)
+        self.setInitialPosition(closer.position)
         return self.estimated_position
 
     def getCloserBeacon(self):
         measurements = self.getAllMeasurements()
-        valid_beacons = self.sortWithIndeces(measurements)
+        valid_beacons = self.selector.sortWithIndeces(measurements)
         return self.beacons[valid_beacons[0][0]]
 
     def getAllMeasurements(self):
@@ -52,20 +59,20 @@ class EKF(object):
         return self.estimated_position
 
     def prediction(self):
-        self.estimated_position = self.F * self.estimated_position
-        estimated_cov_matrix = self.F * self.cov_matrix * np.transpose(self.F) + self.Ex
-        measurements = self.selectiveExtraction(estimated_cov_matrix)
+        self.estimated_position = multiply(self.F, self.estimated_position)
+        estimated_cov_matrix = multiply(self.F, self.cov_matrix, transpose(self.F)) + self.Ex
+        measurements = self.selectiveMeasurements(estimated_cov_matrix)
 
         h = np.zeros(self.sensor_size)
         for i in range(0, self.sensor_size):
             if measurements[i] != 0:
-                h[i] = self.space_to_value(self.estimated_position[0] - self.beacons[i].position)
+                h[i] = self.model.spaceToValue(self.estimated_position[0] - self.beacons[i].position)
 
         # H is the measurement equation
         H = np.empty((0, 4))
         for i in range(0, len(measurements)):
             if measurements[i] != 0:
-                dh_dx, dh_dy = self.derivative(self.estimated_position[0] - self.beacons[i].position)
+                dh_dx, dh_dy = self.model.derivative(self.estimated_position[0] - self.beacons[i].position)
             else:
                 dh_dx, dh_dy = 0.0, 0.0
             H = np.append(H, np.array([[dh_dx, dh_dy, 0.0, 0.0]]), axis=0)
@@ -73,41 +80,19 @@ class EKF(object):
         return H, h, measurements, estimated_cov_matrix
 
     def correction(self, H, h, measurements, estimated_cov_matrix):
-        K = estimated_cov_matrix * np.transpose(H) * np.invert(H * estimated_cov_matrix * np.transpose(H) + self.Ez)
-        self.estimated_position += K * (measurements - h)
-        self.cov_matrix = (np.eye(N=4) - K * H) * estimated_cov_matrix
-        self.prediction_sequence.append(np.transpose(self.estimated_position))
+        H_hat = multiply(H, estimated_cov_matrix, transpose(H))
+        K = multiply(estimated_cov_matrix, transpose(H), invert(H_hat + self.Ez))
 
-    def selectiveExtraction(self, estimated_cov_matrix):
-        indexes = self.selectPosition(estimated_cov_matrix)
-        measurements = []
+        self.estimated_position += multiply(K, (measurements - h))
+        self.cov_matrix = multiply((np.eye(N=4) - multiply(K, H)), estimated_cov_matrix)
+        self.prediction_sequence.append(transpose(self.estimated_position))
+
+    def selectiveMeasurements(self, estimated_cov_matrix):
+        indexes = self.selector.selectBestPositions(estimated_cov_matrix, self.estimated_position)
+
+        measurements = np.zeros(self.sensor_size)
         for i in indexes:
             data = self.client.pollBasestation(self.beacons[i].address)
-            measurements += [extractRSSIForTag(self.tag, data)]
+            measurements[i] = extractRSSIForTag(data, self.tag)
+
         return measurements
-
-    def selectPosition(self, estimated_cov_matrix):
-        estimated_cov_matrix_inv = np.invert(estimated_cov_matrix)
-        distances = []
-        for i in self.sensor_size:
-            extended_beacon_pos = np.append(self.beacons[i].position, np.array([0, 0]))
-            difference = np.transpose(self.estimated_position) - extended_beacon_pos
-            distances += difference * (estimated_cov_matrix_inv * np.transpose(difference))
-
-        valid_distances = self.sortWithIndeces(distances)
-        return [valid_distances[i][0] for i in range(0, min(3, len(valid_distances)))]
-
-    def derivative(self, arr):
-        dx = self.model_coeffs[0] * 1.5 * np.sqrt(arr * arr) * 2 * arr[0] + self.model_coeffs[1] * 2 * arr[0] + \
-                    self.model_coeffs[2] * arr[0] * 1 / np.sqrt(arr * arr)
-        dy = self.model_coeffs[0] * 1.5 * np.sqrt(arr * arr) * 2 * arr[1] + self.model_coeffs[1] * 2 * arr[1] + \
-                    self.model_coeffs[2] * arr[1] * 1 / np.sqrt(arr * arr)
-        return dx, dy
-
-    def space_to_value(self, arr):
-        return self.model_coeffs[0] * (np.sqrt(arr * arr)) ** 3 + self.model_coeffs[1] * (arr * arr) + \
-                    self.model_coeffs[2] * np.sqrt(arr * arr) + self.model_coeffs[4]
-
-    def sortWithIndeces(self, list):
-        sorted_tuples = sorted(enumerate(list), key=lambda x: x[1])
-        return [item for item in sorted_tuples if item[1] > 0]
--- code/client_python/src/position_publisher/src/localization_node.py
+++ code/client_python/src/position_publisher/src/localization_node.py
@@ -1,21 +1,28 @@
+#! /usr/bin/env python
+
 import rospy
-from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Quaternion
 
+from model import Poly3
+from beacon_selector import BeaconSelector
 from ekf import EKF
 from basestation import Basestation
 from json_extractor import NoMeasurementException
 
+# (-0.1416)  2.311  -14.76  36.79;
+COEFFS = [-0.1416, 2.311, -14.76, 36.79]
+
 # (-1.416e-07)*(sqrt(x.^2 + y.^2)).^3 + 0.0002311*(x.^2 + y.^2) + -0.1476*sqrt(x.^2 + y.^2) + 36.79
-COEFFS = [-1.416e-07, 0.0002311, -0.1476, 36.79]
+#COEFFS = [-1.416e-07, 0.0002311, -0.1476, 36.79]
 
 EVERY_MINUTE = 0.1667
 EVERY_SECOND = 1
 
 
 def createBeaconList():
-    return [ Basestation("192.168.1.19", 0, 0)
+    return [ Basestation("192.168.1.19", 2, 2),
+                Basestation("192.168.1.17", 3, 3)
             ]
 
 
@@ -28,28 +35,31 @@ def publishPosition(position, publisher):
     msg.pose.pose.position = Point(position[0], position[1], 0)
 
     publisher.publish(msg)
-    rospy.loginfo(msg)
 
 
 def runNode():
-    pub = rospy.Publisher('odom', String, queue_size=10)
+    pub = rospy.Publisher('odom', Odometry, queue_size=10)
     rospy.init_node('position_publisher', anonymous=True)
     rate = rospy.Rate(EVERY_SECOND)
-    
-    ekf = EKF("2022", 1, 1, 0.1, createBeaconList(), COEFFS)
+
+    beacons = createBeaconList()
+    model = Poly3(COEFFS)
+    selector = BeaconSelector(beacons)
+    ekf = EKF("2022", 1, 0.1, beacons, model, selector)
     miss = 2
 
     while not rospy.is_shutdown():
-
         try:
             if miss >= 2:            # after two misses it is assumed the target is not in range
                 rate = rospy.Rate(EVERY_MINUTE)  # once in a minute
                 position = ekf.setInitialPositionToCloserBeacon()
             else:
-                rate = rospy.Rate(EVERY_SECOND)  # normal rate
+                print "ekf"
                 position = ekf.ekf()
+                print "end ekf", position
 
             publishPosition(position, pub)
+            rate = rospy.Rate(EVERY_SECOND)  # normal rate
             miss = 0
 
         except NoMeasurementException:      # no measurements means the target is not in range
