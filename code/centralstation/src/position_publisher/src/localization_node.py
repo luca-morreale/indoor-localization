f964c4ece6639d159df5ab9e402a8645271399a8
Luca Morreale <luca.morreale93@gmail.com>
2016-11-02 22:57:25 +0100

    Refactored localization node

--- code/centralstation/src/position_publisher/src/localization_node.py
+++ code/centralstation/src/position_publisher/src/localization_node.py
@@ -10,62 +10,67 @@ from ekf import EKF
 from basestation import Basestation
 from json_extractor import NoMeasurementException
 
-# model in meters
-COEFFS = [-0.1416, 2.311, -14.76, 36.79]
 
-# model in centimeters
-# COEFFS = [-1.416e-07, 0.0002311, -0.1476, 36.79]
+class LocalizationNode(object):
+    # model in meters
+    COEFFS = [-0.1416, 2.311, -14.76, 36.79]
 
-EVERY_MINUTE = 0.1667
-EVERY_SECOND = 1
+    EVERY_MINUTE = 0.1667
+    EVERY_SECOND = 1
 
-def createBasestationList():
-    return [ Basestation("192.168.1.19", 2, 2),
-                Basestation("192.168.1.17", 3, 3)
-            ]
+    def __init__(self):
+        rospy.init_node('position_publisher', anonymous=True)
+        self.publisher = rospy.Publisher('odom', Odometry, queue_size=10)
+        self.rate = rospy.Rate(LocalizationNode.EVERY_SECOND)
 
+        self.extractParams()
+        self.model = Poly3(LocalizationNode.COEFFS)
+        self.selector = BasestationSelector(self.basestations)
+        self.ekf = EKF(self.tag, 1, 0.1, self.basestations, self.model, self.selector)
+        self.miss_counter = 2
 
-def publishPosition(position, publisher):
-    msg = Odometry()
-    msg.header.stamp = rospy.Time.now()
-    msg.header.frame_id = '/odom'
-    msg.child_frame_id = '/base_link'
+    def extractParams(self):
+        self.tag = rospy.get_param("/localization_node/tag")
+        stations = rospy.get_param("/localization_node/basestations")
+        self.buildBasestation(stations)
 
-    msg.pose.pose.position = Point(position[0], position[1], 0)
+    def buildBasestation(self, stations):
+        self.basestation = []
+        for station in stations:
+            self.basestation.append(Basestation(station[0], station[1], station[2]))
 
-    publisher.publish(msg)
+    def publishPosition(self, position):
+        msg = Odometry()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = '/odom'
+        msg.child_frame_id = '/target_' + self.tag
 
+        msg.pose.pose.position = Point(position[0], position[1], 0)
+        self.publisher.publish(msg)
 
-def runNode():
-    pub = rospy.Publisher('odom', Odometry, queue_size=10)
-    rospy.init_node('position_publisher', anonymous=True)
-    rate = rospy.Rate(EVERY_SECOND)
+    def localizationLoop(self):
+        while not rospy.is_shutdown():
+            try:
+                if self.miss >= 2:  # after two misses it is assumed the target is not in range
+                    self.rate = rospy.Rate(LocalizationNode.EVERY_MINUTE)  # once in a minute
+                    position = self.ekf.setInitialPositionToCloserBasestation()
+                else:
+                    position = self.ekf.ekfIteration()
+                    print position
 
-    basestations = createBasestationList()
-    model = Poly3(COEFFS)
-    selector = BasestationSelector(basestations)
-    ekf = EKF("2022", 1, 0.1, basestations, model, selector)
-    miss = 2
+                self.publishPosition(position)
+                self.rate = rospy.Rate(LocalizationNode.EVERY_SECOND)  # normal rate
+                self.miss = 0
 
-    while not rospy.is_shutdown():
-        try:
-            if miss >= 2:            # after two misses it is assumed the target is not in range
-                rate = rospy.Rate(EVERY_MINUTE)  # once in a minute
-                position = ekf.setInitialPositionToCloserBasestation()
-            else:
-                position = ekf.ekfIteration()
+            except NoMeasurementException:  # no measurements means the target is not in range
+                self.miss += 1
 
-            publishPosition(position, pub)
-            rate = rospy.Rate(EVERY_SECOND)  # normal rate
-            miss = 0
+            self.rate.sleep()
 
-        except NoMeasurementException:      # no measurements means the target is not in range
-            miss += 1
-
-        rate.sleep()
 
 if __name__ == '__main__':
     try:
-        runNode()
+        node = LocalizationNode()
+        node.localizationLoop()
     except rospy.ROSInterruptException:
         pass
