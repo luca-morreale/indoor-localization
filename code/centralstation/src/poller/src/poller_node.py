46e70e2815db39ba3690d6eb4e52611fe5c99ad6
Luca Morreale <luca.morreale93@gmail.com>
2016-11-13 01:18:15 +0100

    Moved poller node and added usage of custom messages

--- code/centralstation/src/poller/src/poller_node.py
+++ code/centralstation/src/poller/src/poller_node.py
@@ -1,25 +1,28 @@
+#! /usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
+from poller.msg import Measurement, MeasurementList
 
 import socket
 
 from client import Client
 from orderedset import OrderedSet
 from basestation import Basestation
-from json_verifier import containsMeasurements
+from json_handler import containsMeasurements, extractJson
 
 
 class Poller(object):
     def __init__(self):
         rospy.init_node('poller_node')
-        self.rate = rospy.Rate(3)  # 3hz
+        self.rate = rospy.Rate(3)   # 3hz
         self.extractBasestationFromParams()
         self.createCommunicators()
         self.request_list = OrderedSet([])
 
     def createCommunicators(self):
         self.client = Client(10019)
-        self.measurements_publisher = rospy.Publisher('measurements', String, queue_size=10)
+        self.measurements_publisher = rospy.Publisher('measurements', MeasurementList, queue_size=10)
         self.request_subscriber = rospy.Subscriber("measurements_request", String, self.pushbackRequest)
 
     def extractBasestationFromParams(self):
@@ -32,7 +35,14 @@ class Poller(object):
             self.basestations.append(Basestation(station[0], float(station[1]), float(station[2])))
 
     def pushbackRequest(self, msg):
-        self.request_list.append(msg.data)
+        self.request_list.add(msg.data)
+
+    def measurementsLoop(self):
+        while not rospy.is_shutdown():
+            while not self.request_list.isEmpty():
+                station_address = self.request_list.pop()
+                self.serveRequest(station_address)
+            self.rate.sleep()
 
     def pollStation(self, station_address):
         return self.client.pollBasestation(station_address)
@@ -41,16 +51,24 @@ class Poller(object):
         try:
             data = self.pollStation(station_address)
             if containsMeasurements(data):
-                self.measurements_publisher.publish(data)
+                self.publishMeasuements(extractJson(data), station_address)
         except socket.error:
             pass
 
-    def measurementsLoop(self):
-        while not rospy.is_shutdown():
-            while not self.request_list.isEmpty():
-                station_address = self.request_list.pop()
-                self.serveRequest(station_address)
-            self.rate.sleep()
+    def publishMeasuements(self, measurs, station):
+        msg = MeasurementList()
+        for el in measurs:
+            msg.data.append(self.generateMeasurement(el))
+        msg.basestation = station
+        msg.header.stamp = rospy.Time.now()
+        self.measurements_publisher.publish(msg)
+
+    def generateMeasurement(self, element):
+        tmp = Measurement()
+        tmp.tag = element['id_tag'].encode('utf-8')
+        tmp.measurement = int(element['rssid'])
+        return tmp
+
 
 if __name__ == '__main__':
     try:
