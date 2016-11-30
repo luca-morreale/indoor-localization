aa7fa4c82c589ca92d274f80757d8d4176bb635d
Luca Morreale <luca.morreale93@gmail.com>
2016-11-30 15:39:49 +0100

    Transformed selector from class to functions

--- code/centralstation/src/position_publisher/src/basestation_selector.py
+++ code/centralstation/src/position_publisher/src/basestation_selector.py
@@ -2,23 +2,25 @@
 import numpy as np
 from matrix_operations import transpose, invert, multiply
 
+def selectBestPositions(sensors, estimated_cov_matrix, estimated_position, size):
+    sensor_size = len(sensors)
+    size = sensor_size if size < 0 else size
+    inv_estimated_cov_matrix = invert(estimated_cov_matrix)
+    distances = np.ones(sensor_size) * -1
+    extended_sensors_pos = extendSensorPosition(sensors)
+    for i in range(sensor_size):
+        difference = transpose(estimated_position) - extended_sensors_pos[i]
+        distances[i] = multiply(difference, inv_estimated_cov_matrix, transpose(difference))
 
-class BasestationSelector(object):
-    def __init__(self, basestations):
-        self.basestations = basestations
-        self.sensor_size = len(basestations)
+    valid_distances = sortWithIndeces(distances)
+    return [valid_distances[i][0] for i in range(0, min(size, len(valid_distances)))]
 
-    def selectBestPositions(self, estimated_cov_matrix, estimated_position, size=5):
-        estimated_cov_matrix_inv = invert(estimated_cov_matrix)
-        distances = np.ones(self.sensor_size) * -1
-        for i in range(self.sensor_size):
-            extended_basestation_pos = np.append(self.basestations[i].position, np.array([0, 0]))
-            difference = transpose(estimated_position) - extended_basestation_pos
-            distances[i] = multiply(difference, estimated_cov_matrix_inv, transpose(difference))
+def extendSensorPosition(sensors):
+    extended_sensors = []
+    for sensor in sensors:
+        extended_sensors.append(np.append(sensor.position, np.array([0, 0])))
+    return extended_sensors
 
-        valid_distances = self.sortWithIndeces(distances)
-        return [valid_distances[i][0] for i in range(0, min(size, len(valid_distances)))]
-
-    def sortWithIndeces(self, list):
-        sorted_tuples = sorted(enumerate(list), key=lambda x: x[1])
-        return [item for item in sorted_tuples if item[1] > 0]
+def sortWithIndeces(list):
+    sorted_tuples = sorted(enumerate(list), key=lambda x: x[1])
+    return [item for item in sorted_tuples if item[1] > 0]
