8abcaf1378dfde6eb19ac3f38effb34ec0a079d2
Luca Morreale <luca.morreale93@gmail.com>
2016-12-02 17:30:39 +0100

    Minor changes

--- code/centralstation/src/position_publisher/src/basestation.py
+++ code/centralstation/src/position_publisher/src/basestation.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import rospy
 from geometry_msgs.msg import PointStamped
@@ -7,12 +8,11 @@ from geometry_msgs.msg import Point, Quaternion
 class Basestation():
     def __init__(self, address, x, y):
         self.address = address
+        self.id = address.split(str=".")[-1]
         self.position = np.array([x, y])
         self.frame = "basestation"
+        self.custom_frame = self.frame + '_' + self.id
         self.publisher = rospy.Publisher(self.frame, PointStamped, queue_size=10)
-
-    def extractID(self, address):
-        self.id = address.split(str=".")[-1]
 
     def distanceTo(self, x, y):
         self.distance(np.array([x, y]))
@@ -23,7 +23,7 @@ class Basestation():
     def publishPosition(self):
         msg = PointStamped()
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = self.frame + '_' + self.id
+        msg.header.frame_id = self.custom_frame
 
         msg.point = Point(self.position[0], self.position[1], 0)
         self.publisher.publish(msg)
