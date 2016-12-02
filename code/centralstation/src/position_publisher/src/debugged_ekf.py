d06fad611c3f8c37dec9d74c4562893c1394920f
Luca Morreale <luca.morreale93@gmail.com>
2016-12-02 17:32:54 +0100

    Created extention of EKF that publish debug circumference

---
+++ code/centralstation/src/position_publisher/src/debugged_ekf.py
@@ -0,0 +1,9 @@
+
+from ekf import EKF
+
+
+class DebuggedEKF(EKF):
+
+    def updatePosition(self, data, id_station, current_time):
+        EKF.updatePosition(self, data, id_station, current_time)
+        self.basestations[id_station].publishPosition(data, self.tag)
