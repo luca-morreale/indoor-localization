7687bd168c11e7b33a18d5d3125b983bdb5c33d3
Luca Morreale <luca.morreale93@gmail.com>
2016-12-02 15:41:27 +0100

    Changed indentation

--- code/centralstation/src/position_publisher/src/basestation.py
+++ code/centralstation/src/position_publisher/src/basestation.py
@@ -5,22 +5,25 @@ from geometry_msgs.msg import Point, Quaternion
 
 
 class Basestation():
-	def __init__(self, address, x, y):
-		self.address = address
-		self.position = np.array([x, y])
-		self.frame = "basestation"
-		self.publisher = rospy.Publisher(self.frame, PointStamped, queue_size=10)
+    def __init__(self, address, x, y):
+        self.address = address
+        self.position = np.array([x, y])
+        self.frame = "basestation"
+        self.publisher = rospy.Publisher(self.frame, PointStamped, queue_size=10)
 
-	def distanceTo(self, x, y):
-		self.distance(np.array([x, y]))
+    def extractID(self, address):
+        self.id = address.split(str=".")[-1]
 
-	def distance(self, obj_pos):
-		return np.sqrt(self.position * obj_pos)
+    def distanceTo(self, x, y):
+        self.distance(np.array([x, y]))
 
-	def publishPosition(self):
-		msg = PointStamped()
-		msg.header.stamp = rospy.Time.now()
-		msg.header.frame_id = self.frame
+    def distance(self, obj_pos):
+        return np.sqrt(self.position * obj_pos)
 
-		msg.point = Point(self.position[0], self.position[1], 0)
-		self.publisher.publish(msg)
+    def publishPosition(self):
+        msg = PointStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = self.frame + '_' + self.id
+
+        msg.point = Point(self.position[0], self.position[1], 0)
+        self.publisher.publish(msg)
