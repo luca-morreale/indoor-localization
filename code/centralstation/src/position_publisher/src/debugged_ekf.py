8e7d5e855af239d0a19891ca3403e83364eac04a
Luca Morreale <luca.morreale93@gmail.com>
2016-12-13 17:17:24 +0100

    Removed extension of localization node

--- code/centralstation/src/position_publisher/src/debugged_ekf.py
+++
@@ -1,9 +0,0 @@
-
-from ekf import EKF
-
-
-class DebuggedEKF(EKF):
-
-    def updatePosition(self, data, id_station, current_time):
-        EKF.updatePosition(self, data, id_station, current_time)
-        self.basestations[id_station].publishMarker(data, self.tag)
